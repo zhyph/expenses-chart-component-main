409e34ae792ab688288f7baea1cbf2d9d470bf12
Artur <artur.almeida1@outlook.com>
2022-06-06 22:39:05 -0300

    Initial Commit

---
+++ src/Components/Attribution.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+const Attribution = () => {
+  return (
+    <div className="attribution">
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Your Name Here</a>.
+    </div>
+  );
+};
+
+export default Attribution;
